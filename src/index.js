849bc9daa34b9884dd8c27f84bfcf103b2501180
vedant <ursvedantyetekar@gmail.com>
2022-04-18 22:41:38 +0530

    started implementing redux

--- src/index.js
+++ src/index.js
@@ -1,27 +1,26 @@
 import React from "react";
 import { render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-
 import App from "./App";
-import { UserProvider } from "./contexts/user.context";
 import { CategoriesProvider } from "./contexts/categories.context";
 import { CartProvider } from "./contexts/cart.context";
-
 import "./index.scss";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
 
 const rootElement = document.getElementById("root");
 
 render(
   <React.StrictMode>
-    <BrowserRouter>
-      <UserProvider>
+    <Provider store={store}>
+      <BrowserRouter>
         <CategoriesProvider>
           <CartProvider>
             <App />
           </CartProvider>
         </CategoriesProvider>
-      </UserProvider>
-    </BrowserRouter>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   rootElement
 );
