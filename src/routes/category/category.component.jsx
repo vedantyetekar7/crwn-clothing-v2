aae4d8243552c272157ce00d0d912c5d3c733fe6
vedant <ursvedantyetekar@gmail.com>
2022-04-24 22:58:23 +0530

    minor fixes

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,17 +1,16 @@
-import { useState, useEffect, Fragment } from 'react';
-import { useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import { useState, useEffect, Fragment } from "react";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
 
-import ProductCard from '../../components/product-card/product-card.component';
+import ProductCard from "../../components/product-card/product-card.component";
 
-import { selectCategoriesMap } from '../../store/categories/categories.selector';
+import { selectCategoriesMap } from "../../store/categories/categories.selector";
 
-import { CategoryContainer, Title } from './category.styles';
+import { CategoryContainer, Title } from "./category.styles";
 
 const Category = () => {
   const { category } = useParams();
   const categoriesMap = useSelector(selectCategoriesMap);
-  console.log('rendering category');
   const [products, setProducts] = useState(categoriesMap[category]);
 
   useEffect(() => {
