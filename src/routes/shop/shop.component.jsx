6d7f6905bed42b7bea862363b7f8f9e19e27e471
vedant <ursvedantyetekar@gmail.com>
2022-06-08 13:18:12 +0530

    implemented createSelector login in category selector

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -2,20 +2,18 @@ import { useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 
-import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
-
-import { setCategories } from '../../store/categories/categories.action';
-
 import CategoriesPreview from '../categories-preview/categories-preview.component';
 import Category from '../category/category.component';
+import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
+import { setCategories } from '../../store/categories/category.action';
 
 const Shop = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
     const getCategoriesMap = async () => {
-      const categories = await getCategoriesAndDocuments('categories');
-      dispatch(setCategories(categories));
+      const categoriesArray = await getCategoriesAndDocuments('categories');
+      dispatch(setCategories(categoriesArray));
     };
 
     getCategoriesMap();
--- src/store/categories/categories.selector.js
+++
@@ -1,6 +0,0 @@
-export const selectCategoriesMap = (state) => {
-  return state.categories.categories.reduce((acc, { title, items }) => {
-    acc[title.toLowerCase()] = items;
-    return acc;
-  }, {});
-};
